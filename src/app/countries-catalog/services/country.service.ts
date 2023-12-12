da17364511359874eb99412cd258946954185c08
Seyha Vorn <vornseyha4758@gmail.com>
2023-12-12 11:12:57 +0700

    create Country Modules

---
+++ src/app/countries-catalog/services/country.service.ts
@@ -0,0 +1,15 @@
+import { Injectable } from '@angular/core';
+import { Observer } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class CountryService {
+  // constructor(
+  //   private http: HttpClient
+  // ) {}
+
+  // getCountry(): Observer<any[]> {
+
+  // }
+}
